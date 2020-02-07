1882b07b1101646f1e3b6f9cb2315eac80da3aca
NOt3vil <k@doesnotexist.com>
2020-02-07 08:14:45 +0530

    Added credential storing capabillities

--- main.py
+++ main.py
@@ -14,62 +14,102 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+# TODO: Make this in its own database opening function
+# TODO: Decide if one or two dbs are required
 db = sqlite3.connect("Pass.sqlite")
 cur = db.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS accounts(user TEXT,hash TEXT,salt TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS passwords(name TEXT,data TEXT,salt TEXT)')
+db.commit()
 
 
-# def storeCred(name, pasw, note):
-
-def derKey(salt, key, enc):
-    if enc:
-        salt = os.urandom(16)
-    keyb = key.encode('utf-8')
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-        backend=default_backend()
-    )
-    pash = base64.urlsafe_b64encode(kdf.derive(keyb))
-    f = Fernet(pash)
-    if enc:
-        return f, salt
-    else:
-        return f
+def derKey (salt, key, enc):
+	if enc:
+		salt = os.urandom(16)
+	keyb = key.encode('utf-8')
+	kdf = PBKDF2HMAC(
+		algorithm=hashes.SHA256(),
+		length=32,
+		salt=salt,
+		iterations=100000,
+		backend=default_backend()
+	)
+	pash = base64.urlsafe_b64encode(kdf.derive(keyb))
+	f = Fernet(pash)
+	if enc:
+		return f, salt
+	else:
+		return f
 
 
-def decrypt(mess, key, non):
-    f = derKey(non, key, False)
-    messb = mess.encode('utf-8')
-    messb = f.decrypt(messb)
-    return messb.decode('utf-8')
+def decrypt (mess, key, non):
+	f = derKey(non, key, False)
+	messb = mess.encode('utf-8')
+	messb = f.decrypt(messb)
+	return messb.decode('utf-8')
 
 
-def encrypt(mess, key):
-    f, salt = derKey(0, key, True)
-    token = f.encrypt(mess.encode('utf-8'))
-    return token.decode('utf-8'), salt
+def encrypt (mess, f):
+	token = f.encrypt(mess.encode('utf-8'))
+	return token.decode('utf-8')
+
+def usrExist(usr):
+	cur.execute("SELECT user FROM accounts")
+	for user in cur.fetchall():
+		if user[0] == usr:
+			return True
+		# print("[D]: user: ",user)
+	return False
+
+def createUsr (usr, pas):
+	# TODO: Check if user already exist
+	h = hashlib.blake2b()
+	salt = os.urandom(8)
+	salt = base64.urlsafe_b64encode(salt)
+	h.update(pas.encode('utf-8'))
+	h.update(salt)
+	pash = h.hexdigest()
+	print(pash, type(salt))
+	cur.execute("INSERT INTO accounts(user, hash, salt) VALUES(?, ?, ?)", (usr, pash, salt))
+	db.commit()
 
 
-def createUsr(usr, pas):
-    h = hashlib.blake2b()
-    salt = os.urandom(8)
-    salt = base64.urlsafe_b64encode(salt)
-    h.update(pas.encode('utf-8'))
-    h.update(salt)
-    pash = h.hexdigest()
-    print(pash, type(salt))
-    cur.execute("INSERT INTO accounts(user, hash, salt) VALUES(?, ?, ?)", (usr, pash, salt))
-    db.commit()
+def verifyUsr (usr, pas):
+	h = hashlib.blake2b()
+	h.update(pas.encode('utf-8'))
+	cur.execute("SELECT salt FROM accounts WHERE user='" + usr + "'")
+	h.update(cur.fetchone()[0])
+	cur.execute("SELECT hash FROM accounts WHERE user='" + usr + "'")
+	if h.hexdigest() == cur.fetchone()[0]:
+		return True
+	return False
 
 
-# def verifyUsr(usr,pass):
+# TODO: INFO: note as a csv, make an note structure
+# TODO: Test
+def storeCred(key, name, pasw, note = "NULL"):
+	if note == "NULL":
+		note = ""
+		note = "pass:" + passw
+	else:
+		note = "pass:" + passw + ",note:" + note
+	
+	f, salt = derKey(0, key, True)
+	note = encrypt(note,f)
+	cur.execute("INSERT INTO passwords(name, data, salt) VALUES(?, ?, ?)", (name, note, salt))
+	db.commit()
 
-pas = str(input("Pass: "))
-usr = str(input("Mes: "))
-createUsr(usr, pas)
 
-cur.close()
-db.close()
+print("Enter the user info")
+usr = str(input("Username: "))
+pas = str(input("Password: "))
+if usrExist(usr):
+	print(" [I] user already exist going forward with verification!")
+else:
+	createUsr(usr, pas)
+print(" [I] verifing user")
+auth = verifyUsr(usr, pas)
+if not auth:
+	print(" [E] Authentication Failure\n [E] Wrong username or password")
+	exit
+print(" [I] Authentication Succesfull\n\tAcess granted!")
